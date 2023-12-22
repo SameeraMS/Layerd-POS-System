bc131ae35e12577c7e50f4c1100bea619f8a7a87
Sameera Madushan Silva <madushansameera499@gmail.com>
2023-12-22 14:05:32 +0530

    bo

--- src/main/java/com/example/layeredarchitecture/controller/ManageItemsFormController.java
+++ src/main/java/com/example/layeredarchitecture/controller/ManageItemsFormController.java
@@ -1,6 +1,8 @@
 package com.example.layeredarchitecture.controller;
 
 
+import com.example.layeredarchitecture.bo.BOFactory;
+import com.example.layeredarchitecture.bo.custom.CustomerBO;
 import com.example.layeredarchitecture.bo.custom.ItemBO;
 import com.example.layeredarchitecture.bo.custom.impl.ItemBOImpl;
 import com.example.layeredarchitecture.model.ItemDTO;
@@ -37,7 +39,7 @@ public class ManageItemsFormController {
     public TableView<ItemTM> tblItems;
     public TextField txtUnitPrice;
     public JFXButton btnAddNewItem;
-    ItemBO itemBO = new ItemBOImpl();
+    ItemBO itemBO = (ItemBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.ITEM);
     public void initialize() {
         tblItems.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("code"));
         tblItems.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("description"));
